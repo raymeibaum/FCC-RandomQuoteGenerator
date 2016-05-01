c2dc7d164066be438e64e76d039328a5b321302c
Ray Meibaum <rmeibaum@me.com>
2016-05-01 18:22:50 -0500

    coppied over from codepen. ready for debugging

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+  $.get("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=25&callback=", function(quotes) {
+    displayQuote(quotes);
+    $('#newQuote').on('click', function(event) {
+      event.preventDefault();
+      displayQuote(quotes);
+    });
+  });
+});
+
+function displayQuote(quotes) {
+  var randomNum = Math.floor(Math.random() * (quotes.length + 1));
+  $("#quote").html(quotes[randomNum].content).append($('<footer/>').text(quotes[randomNum].title));
+  $('#tweetQuote').attr({
+    href: "https://twitter.com/intent/tweet?text=" + parseQuote(quotes[randomNum].content) + ' -' + quotes[randomNum].title
+  })
+};
+
+function parseQuote(quote) {
+  return quote.slice(3, -5);
+}
